83aeca81d32b68bc8a26939bf4eb625c24e6d523
TeacherFC <fernando.casas@uwiener.edu.pe>
2022-09-14 20:26:24 -0500

    Condicional IF

---
+++ Sesion4-Estructuras-Control/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int nNumero = -1000;
+Double nnDecimal = 12.01;
+if (nNumero > 12)
+    Console.WriteLine("El numero es mayor a 12");
+else
+    Console.WriteLine("El numero es menor o igual a 12");
+
+Thread.Sleep(10000); //Espera 10 seg = 10000 Milisegundos
